519d7736eb024d06e986d0b82c992a16f33932a6
erikabracamonte15 <erika.bracamonte15@gmail.com>
2022-05-13 11:58:43 +0200

    iniciando git

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+
+const NavBar = () =>{
+    return (
+        <nav>
+            <h1>ECommerce</h1>
+            <div>
+                <button>Celulares</button>
+                <button>Tablets</button>
+                <button>Notebooks</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
